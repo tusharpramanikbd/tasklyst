35834194e917aeb26fe7dc31963dc97f2051a644
Tushar Pramanik <tusharpramanikbd@gmail.com>
2025-02-15 14:00:37 +0600

    updated gitignore, formatted app.tsx

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/** @format */
+
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Open up App.tsx to start working on your app!</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +17,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
