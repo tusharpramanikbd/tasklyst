a0ff5d75c451e8053a1a451e0ebeb0ce3e37891b
Tushar Pramanik <tusharpramanikbd@gmail.com>
2025-02-15 15:12:45 +0600

    installed and implemented safe area view

--- App.tsx
+++ App.tsx
@@ -2,21 +2,34 @@
 
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    // for ios, we need to wrap the app content in a SafeAreaProvider
+    <SafeAreaProvider>
+      <AppContent />
+    </SafeAreaProvider>
   );
 }
 
+const AppContent = () => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <StatusBar style="auto" />
+      <View style={styles.container}>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  container: {
+    flex: 1,
   },
 });
