7c48005f820b17a2877c510718817e5e1dd85893
Tushar Pramanik <tusharpramanikbd@gmail.com>
2025-02-15 14:23:21 +0600

    installed eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+/** @format */
+
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ["expo", "prettier"],
+  ignorePatterns: ["/dist/*"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 /** @format */
 
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -9,14 +9,14 @@ export default function App() {
       <StatusBar style="auto" />
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
