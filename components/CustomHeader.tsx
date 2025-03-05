35d45996966fcb86107b058c1ca752550052120a
Tushar Pramanik <tusharpramanikbd@gmail.com>
2025-03-05 12:41:40 +0600

    added linear gradient, set theme mode to light

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -1,18 +1,25 @@
 /** @format */
 
-import { View, Text, StyleSheet } from "react-native";
+import { Text, StyleSheet } from "react-native";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import { useGetHeaderHeight } from "@hooks/useGetHeaderHeight";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function CustomHeader({ title }: { title: string }) {
   const height = useGetHeaderHeight();
 
   return (
-    <View style={[styles.header, { height: height }]}>
+    <LinearGradient
+      colors={["#667eea", "#764ba2"]}
+      style={[styles.header, { height: height }]}
+      locations={[0, 1]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 0 }}
+    >
       <AntDesign name="left" size={24} color="white" />
       <Text style={styles.title}>{title}</Text>
       <AntDesign name="right" size={24} color="white" />
-    </View>
+    </LinearGradient>
   );
 }
 
@@ -23,7 +30,6 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     paddingHorizontal: 20,
     paddingBottom: 10,
-    backgroundColor: "#6200ee",
   },
   title: {
     color: "#fff",
--- hooks/useThemeColor.ts
+++ hooks/useThemeColor.ts
@@ -1,14 +1,12 @@
 /** @format */
 
-import { useColorScheme } from "react-native";
-
 import { theme } from "@constants/theme";
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
   colorName: keyof typeof theme.light & keyof typeof theme.dark,
 ) {
-  const colorMode = useColorScheme() || "light";
+  const colorMode = "light";
   const colorFromProps = props[colorMode];
 
   if (colorFromProps) {
