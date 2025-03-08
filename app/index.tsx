0be8dcb89e21c6f4c08bf3637f3bd1f165ae7e2e
Tushar Pramanik <tusharpramanikbd@gmail.com>
2025-03-08 22:54:29 +0600

    added scroll view

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,41 @@
 /** @format */
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 import ViewWrapper from "@/components/ViewWrapper";
 import TaskItem from "@/components/TaskItem/TaskItem";
 
 export default function Home() {
   return (
     <ViewWrapper>
-      <View className="flex-1 bg-white m-8 mt-4 rounded-lg p-6 gap-4">
-        <TaskItem />
-        <TaskItem />
-        <TaskItem />
+      <View className="flex-1 bg-white m-8 mt-4 rounded-lg p-6 overflow-hidden">
+        <ScrollView
+          bounces={false}
+          contentContainerClassName="gap-8"
+          showsVerticalScrollIndicator={false}
+        >
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
+        </ScrollView>
       </View>
     </ViewWrapper>
   );
