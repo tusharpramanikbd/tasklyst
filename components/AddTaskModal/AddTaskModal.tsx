3b1c74297e5e99b5e54d7bbd6fbaed920f69dbd7
Tushar Pramanik <tusharpramanikbd@gmail.com>
2025-03-16 18:36:15 +0600

    create task button validation

--- components/AddTaskModal/AddTaskModal.tsx
+++ components/AddTaskModal/AddTaskModal.tsx
@@ -1,7 +1,7 @@
 /** @format */
 
 import { View, Pressable, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import CustomBottomSheetModal from "../BottomSheetModal/CustomBottomSheetModal";
 import { BottomSheetModal, BottomSheetTextInput } from "@gorhom/bottom-sheet";
 import { LinearGradient } from "expo-linear-gradient";
@@ -16,6 +16,14 @@ const AddTaskModal = ({
   bottomSheetModalRef,
   handleCloseBottomSheet,
 }: IAddTaskModal) => {
+  const [taskName, setTaskName] = useState("");
+
+  const handleCreateTask = () => {
+    // TODO: Create task
+    setTaskName("");
+    handleCloseBottomSheet();
+  };
+
   return (
     <CustomBottomSheetModal
       ref={bottomSheetModalRef}
@@ -28,6 +36,8 @@ const AddTaskModal = ({
     >
       <View className="flex-1 gap-6 py-4 z-50">
         <BottomSheetTextInput
+          value={taskName}
+          onChangeText={setTaskName}
           placeholder="Task Name"
           className="border border-gray-300 rounded-md p-2 px-4 min-h-16 text-xl no-underline"
         />
@@ -36,11 +46,12 @@ const AddTaskModal = ({
           locations={[0, 1]}
           start={{ x: 0, y: 0 }}
           end={{ x: 1, y: 0 }}
-          style={styles.button}
+          style={[styles.button, { opacity: taskName.length === 0 ? 0.5 : 1 }]}
         >
           <Pressable
             className="bg-primary p-2 rounded-md"
-            onPress={handleCloseBottomSheet}
+            onPress={handleCreateTask}
+            disabled={taskName.length === 0}
           >
             <Typography type="large" className="text-white">
               Create
